111105f82df8f203f8672e95422caf2f73b0c318
HC <chunhuang@zdongpin.com>
2016-01-21 18:06:55 +0800

    first commit

---
+++ app/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hello from './component.jsx';
+
+function main(){
+    React.render(<h1>asd</h1>,
+        document.getElementById('app')
+    );
+}
+main();
+
+
